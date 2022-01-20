07a1355012080ba4f31c96cf0903776a6ba2c6eb
Nanda Eka <nandaekasuciramadan@gmail.com>
2022-01-20 21:11:41 +0700

    First Commit

---
+++ app/src/main/java/com/example/latihanlinearlayout/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.latihanlinearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.latihanlinearlayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSubmit.setOnClickListener {
+            val firstName = binding.etFirstName.text.toString()
+            val lastName = binding.etLastName.text.toString()
+            val birthDate = binding.etBirthDate.text.toString()
+            val country = binding.etCountry.text.toString()
+            Log.d("MainActivity","$firstName $lastName lahir pada $birthDate di $country formulir disetujui")
+        }
+    }
+}
